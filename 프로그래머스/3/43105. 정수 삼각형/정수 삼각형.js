8e6535d32fd17f6eb4465df9dff7ec169a88a76f
Hyojin <oneNmore.jin@gmail.com>
2024-09-07 15:40:31 +0900

    [level 3] Title: 정수 삼각형, Time: 25.89 ms, Memory: 40.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,14 @@
+function solution(triangle) {
+    for(let i=1;i<triangle.length;i++){
+        for(let j=0;j<triangle[i].length;j++){
+            if(j===0){
+                triangle[i][j]=triangle[i-1][0]+triangle[i][j]
+            }else if(j===triangle[i].length-1){
+                triangle[i][j]=triangle[i-1][triangle[i-1].length-1]+triangle[i][j]
+            }else{
+                triangle[i][j]=Math.max(triangle[i-1][j-1],triangle[i-1][j])+triangle[i][j]
+            }
+        }
+    }
+    return Math.max(...triangle[triangle.length-1])
+}
