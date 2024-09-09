efba91addd8e1f11c46821c5e1cb4c533ad83f37
Hyojin <oneNmore.jin@gmail.com>
2024-09-09 14:18:10 +0900

    [level 2] Title: 전화번호 목록, Time: 387.36 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,13 @@
+function solution(phone_book) {
+    const hashTable={}
+    for (const number of phone_book) {
+        hashTable[number] = true;
+    };
+    for (const number of phone_book) {
+        for (let i = 1; i < number.length; i++) {
+              const prefix = number.substring(0, i);
+              if (hashTable[prefix]) return false;  
+        };
+    };
+    return true
+}
