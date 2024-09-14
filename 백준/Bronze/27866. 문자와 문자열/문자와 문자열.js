5129add6c9e04d97bf6686e7e81cd87d26edfe35
Hyojin <oneNmore.jin@gmail.com>
2024-09-14 22:33:07 +0900

    [Bronze V] Title: 문자와 문자열, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  const idx = Number(input[1]);
+  console.log(input[0][idx - 1]);
+};
+
+solution(input);
