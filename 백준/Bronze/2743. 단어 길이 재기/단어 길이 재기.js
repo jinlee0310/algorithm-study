51564f47e5d40ee927d1c3bb3e18e7ce3fb66d9b
Hyojin <oneNmore.jin@gmail.com>
2024-09-19 18:59:33 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+
+const solution = (input) => {
+  console.log(input.length)
+};
+
+solution(input);
