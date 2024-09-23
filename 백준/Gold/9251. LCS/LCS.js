003efe76c1064675e4e90f77123dd24720da75c0
Hyojin <oneNmore.jin@gmail.com>
2024-09-23 20:32:04 +0900

    [Gold V] Title: LCS, Time: 212 ms, Memory: 24832 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  const str1 = input[0];
+  const str2 = input[1];
+  const dp = Array.from({ length: str2.length + 1 }, () =>
+    Array(str1.length + 1).fill(0),
+  );
+  for (let i = 1; i <= str2.length; i++) {
+    for (let j = 1; j <= str1.length; j++) {
+      if (str2[i - 1] === str1[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  console.log(dp[str2.length][str1.length]);
+};
+
+solution(input);
