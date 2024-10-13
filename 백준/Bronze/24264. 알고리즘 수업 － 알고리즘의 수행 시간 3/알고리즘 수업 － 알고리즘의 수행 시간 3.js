32cad76d94510489763b449e72869b968e1fdc14
LEEHYOJIN <oneNmore.jin@gmail.com>
2024-10-13 13:18:28 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim();
+
+const n=Number(input)
+console.log(Math.pow(n,2))
+console.log(2)
