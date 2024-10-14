b856b681a1c0d807b686326ca99829b36c571296
LEEHYOJIN <oneNmore.jin@gmail.com>
2024-10-14 13:50:09 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim();
+
+const [A,B,C]=input.split(' ').map(Number)
+
+console.log((A+B)%C)
+console.log(((A%C) + (B%C))%C)
+console.log((A*B)%C)
+console.log(((A%C) * (B%C))%C)
