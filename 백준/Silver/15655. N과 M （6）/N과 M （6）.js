591c0ead1c8d0b029235186bec95722fb84f4d20
LEEHYOJIN <oneNmore.jin@gmail.com>
2024-10-25 21:11:09 +0900

    [Silver III] Title: N과 M (6), Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const backtracking = (arr, M, answer = []) => {
+  if (answer.length === M) {
+    return [answer.join(" ")];
+  }
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (answer.length === 0 || answer[answer.length - 1] < arr[i]) {
+      answer.push(arr[i]);
+      result.push(...backtracking(arr, M, answer));
+      answer.pop();
+    }
+  }
+  return result;
+};
+
+const solution = (input) => {
+  const [N, M] = input[0].split(" ").map(Number);
+  const arr = input[1].split(" ").map(Number);
+  arr.sort((a, b) => a - b);
+  const result = backtracking(arr, M);
+  console.log(result.join("\n"));
+};
+
+solution(input);
