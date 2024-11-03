d33e1da161b243a4480f142706c7bd7808793392
LEEHYOJIN <oneNmore.jin@gmail.com>
2024-11-04 02:39:26 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 92 ms, Memory: 9412 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  const n = Number(input[0]);
+  console.log(input[1].slice(n - 5));
+};
+
+solution(input);
