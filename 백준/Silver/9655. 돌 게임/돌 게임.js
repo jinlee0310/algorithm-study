801fb3993b00b4365c3fac37a8c297d0a2eeb2df
LEEHYOJIN <oneNmore.jin@gmail.com>
2024-11-21 16:49:53 +0900

    [Silver V] Title: 돌 게임, Time: 152 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  const N = Number(input);
+  console.log(N % 2 === 0 ? "CY" : "SK");
+};
+
+solution(input);
