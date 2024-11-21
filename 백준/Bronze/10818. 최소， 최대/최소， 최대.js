ebcd366cec0db6ea6d08e6d452d882ec05c792d7
LEEHYOJIN <oneNmore.jin@gmail.com>
2024-11-21 15:01:55 +0900

    [Bronze III] Title: 최소, 최대, Time: 868 ms, Memory: 106548 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  const N = Number(input[0]);
+  const arr = input[1].split(" ").map(Number);
+  arr.sort((a, b) => a - b);
+  console.log(`${arr[0]} ${arr[N - 1]}`);
+};
+
+solution(input);
