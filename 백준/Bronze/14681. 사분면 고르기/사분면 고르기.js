0bb0d254a4c535bdab4c6586dd1f969d236cbc40
LEEHYOJIN <oneNmore.jin@gmail.com>
2024-11-22 16:33:18 +0900

    [Bronze V] Title: 사분면 고르기, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+
+const solution = (input) => {
+  const x = Number(input[0]);
+  const y = Number(input[1]);
+
+  if (x > 0 && y > 0) {
+    console.log(1);
+  } else if (x < 0 && y > 0) {
+    console.log(2);
+  } else if (x < 0 && y < 0) {
+    console.log(3);
+  } else if (x > 0 && y < 0) {
+    console.log(4);
+  }
+};
+solution(input);
