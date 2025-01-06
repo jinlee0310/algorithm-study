2267c80423135fd0e5de4aab08a69c75cfc3762e
LEEHYOJIN <oneNmore.jin@gmail.com>
2025-01-06 18:16:14 +0900

    [Silver V] Title: 분수찾기, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.js
+++ 백준/Silver/1193. 분수찾기/분수찾기.js
@@ -9,38 +9,28 @@ const input = fs
   .toString()
   .trim();
 
-const findSum = (n) => {
-  const dp = Array(100001).fill(0);
-  for (let i = 1; i < dp.length; i++) {
-    dp[i] = (i * (i + 1)) / 2;
-    if (dp[i - 1] <= n && n <= dp[i]) return [i, dp[i - 1] + 1];
+const findDiagonal = (n) => {
+  let i = 1;
+  let sum = 1;
+  while (sum < n) {
+    i++;
+    sum += i;
   }
+  return [i, sum - i + 1];
 };
 
 const solution = (input) => {
   const X = Number(input);
-  // console.log(10000000);
 
-  let [n, st] = findSum(X); // 숫자 총 5개
-  // console.log(n, st);
+  let [n, st] = findDiagonal(X); // 대각선 위치 찾기
   let answer = "";
   if (n % 2 === 1) {
-    let i = n,
-      j = 1;
-    while (st !== X) {
-      i--;
-      j++;
-      st++;
-    }
+    let i = n - (X - st);
+    let j = 1 + (X - st);
     answer = `${i}/${j}`;
   } else {
-    let i = 1,
-      j = n;
-    while (st !== X) {
-      i++;
-      j--;
-      st++;
-    }
+    let i = 1 + (X - st);
+    let j = n - (X - st);
     answer = `${i}/${j}`;
   }
   console.log(answer);
