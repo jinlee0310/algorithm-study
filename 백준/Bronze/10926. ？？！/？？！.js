e51cdbd3eb452627c3259a3a9a83bc29580a8da7
LEEHYOJIN <oneNmore.jin@gmail.com>
2025-01-07 19:23:44 +0900

    [Bronze V] Title: ??!, Time: 100 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+
+console.log(`${input}??!`)
