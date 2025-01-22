1723e141e2a892dc00c909a9b7ef32bb1f241dfb
LEEHYOJIN <oneNmore.jin@gmail.com>
2025-01-22 12:51:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 740 ms, Memory: 94084 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  const [N, M, R] = input[0].split(" ").map(Number);
+  const graph = Array.from({ length: N + 1 }, () => []);
+  for (let i = 1; i < input.length; i++) {
+    const [a, b] = input[i].split(" ").map(Number);
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  for (let node of graph) {
+    node.sort((a, b) => b - a);
+  }
+
+  const visited = Array(N + 1).fill(false);
+  const answer = Array(N).fill(0);
+  let cnt = 0;
+  const dfs = (cur) => {
+    cnt++;
+    answer[cur - 1] = cnt;
+    visited[cur] = true;
+
+    for (let node of graph[cur]) {
+      if (!visited[node]) {
+        dfs(node);
+      }
+    }
+  };
+  dfs(R);
+  console.log(answer.join("\n"));
+};
+
+solution(input);
