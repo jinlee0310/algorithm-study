ee7ea39db5c4e05304f5651ca62d17964a1c3567
LEEHYOJIN <oneNmore.jin@gmail.com>
2025-02-03 00:04:00 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 100 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim();
+
+const [A, B] = input.split(" ").map(BigInt);
+console.log((A + B).toString());
