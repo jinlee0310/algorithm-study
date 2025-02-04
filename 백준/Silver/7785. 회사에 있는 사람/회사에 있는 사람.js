826385a1e7f80afc29eec3a5e026cec4868ca6fb
LEEHYOJIN <oneNmore.jin@gmail.com>
2025-02-04 17:38:00 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 324 ms, Memory: 42176 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  const N = Number(input[0]);
+  const set = new Set();
+
+  for (let i = 1; i <= N; i++) {
+    const [name, order] = input[i].split(" ");
+    if (order === "enter") {
+      set.add(name);
+    } else {
+      set.delete(name);
+    }
+  }
+  const arr = Array.from(set);
+  arr.sort().reverse();
+  console.log(arr.join("\n"));
+};
+
+solution(input);
