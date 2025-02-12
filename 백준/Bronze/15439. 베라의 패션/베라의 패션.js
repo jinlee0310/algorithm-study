50dfec944fef818702c9a951593424c349918425
LEEHYOJIN <oneNmore.jin@gmail.com>
2025-02-12 15:45:50 +0900

    [Bronze IV] Title: 베라의 패션, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim();
+
+const N=Number(input)
+console.log(N*(N-1))
