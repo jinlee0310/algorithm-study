d3f4d0dd70ae4b46dcaf2987a7bdd690e7ab9182
LEEHYOJIN <oneNmore.jin@gmail.com>
2025-03-18 20:25:37 +0900

    [Bronze II] Title: 저항, Time: 156 ms, Memory: 9656 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt"),
+  )
+  .toString()
+  .trim();
+
+const color = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+const [color1, color2, color3] = input.split("\n");
+
+const answer =
+  (color[color1] * 10 + color[color2]) * Math.pow(10, color[color3]);
+console.log(answer);
