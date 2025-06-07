4c2af6b9e681e24964cc5cb1c33232996b54638e
이효진 <oneNmore.jin@gmail.com>
2025-06-07 23:21:40 +0900

    [Bronze V] Title: 아스키 코드, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt")
+  )
+  .toString()
+  .trim();
+
+console.log(input.charCodeAt(0));
