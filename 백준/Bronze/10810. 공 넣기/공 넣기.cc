92c05f7ac91cf6019cba56b1b2dadf82963f2441
이효진 <oneNmore.jin@gmail.com>
2025-08-10 13:50:07 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N,M;
+    cin >> N >> M;
+
+    int arr[101]={};
+    for(int i=0;i<M;i++){
+        int a,b,c;
+        cin >> a >> b >> c;
+        for(int j=a;j<=b;j++){
+            arr[j]=c;
+        }
+    }
+    for(int i=1;i<=N;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
