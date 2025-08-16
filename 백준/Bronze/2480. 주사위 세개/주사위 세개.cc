87f286e3f7da3c72ec755b8aaca5d657c6e70410
이효진 <oneNmore.jin@gmail.com>
2025-08-16 21:12:55 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if(a==b && b==c){
+        cout << 10000+(a*1000);
+    }else if((a==b&&b!=c)){
+        cout << 1000+(a*100);
+    }else if(b==c&&a!=c){
+        cout << 1000+(b*100);
+    }else if(c==a&&b!=c){
+        cout << 1000+(c*100);
+    }else{
+        cout << max({a,b,c})*100;
+    }
+    return 0;
+}
