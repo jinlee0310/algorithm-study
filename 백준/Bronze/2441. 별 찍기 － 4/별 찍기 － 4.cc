797a4b1e102dd009e0ca1e30ede27a43a64f971b
이효진 <oneNmore.jin@gmail.com>
2025-08-30 01:53:17 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            if(i<=j){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
