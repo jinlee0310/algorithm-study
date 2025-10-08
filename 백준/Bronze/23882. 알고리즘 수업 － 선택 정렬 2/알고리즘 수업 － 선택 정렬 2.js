11087841ea2694149bc653bad6aefd61fef2f1af
이효진 <oneNmore.jin@gmail.com>
2025-10-08 14:59:17 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 212 ms, Memory: 12772 KB -BaekjoonHub

---
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt")
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const selectionSort = (arr, k) => {
+  let cnt = 0;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let max = 0;
+    let maxIdx = -1;
+    for (let j = 0; j <= i; j++) {
+      if (arr[j] > max) {
+        max = arr[j];
+        maxIdx = j;
+      }
+    }
+    if (max !== arr[i]) {
+      cnt++;
+
+      const temp = arr[i];
+      arr[i] = max;
+      arr[maxIdx] = temp;
+      if (cnt === k) return arr.join(" ");
+    }
+  }
+  return -1;
+};
+
+const [n, k] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+
+const answer = selectionSort(arr, k);
+console.log(answer);
