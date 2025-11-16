9b9ada970024db89286ec22e78bd2788e66ce351
이효진 <oneNmore.jin@gmail.com>
2025-11-16 21:50:30 +0900

    [Silver V] Title: 소트인사이드, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "/input.txt")
+  )
+  .toString()
+  .trim()
+  .split("")
+  .sort()
+  .reverse()
+  .join("");
+
+console.log(input);
